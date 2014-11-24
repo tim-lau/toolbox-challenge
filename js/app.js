f96e98f474c68736704307767e01179b1c46c4c8
timothyl@uw.edu <Tim@d-140-142-244-48.dhcp4.washington.edu>
2014-11-24 00:29:18 -0800

    game logic working, missing feedback/win screen

--- js/app.js
+++ js/app.js
@@ -5,28 +5,40 @@
 var timer;
 var matches = 0;
 var misses = 0;
-var remaining = 8;
+var remaining = 8
+var pairs = [];
 var tiles = [];
+var plays = 0;
 var i = 0;
-var click;
-var tempImage = null;
-
+var tempTile = [];
+var tempImage = [];
+var twoFlipped = false;
 var gameBoard = $('#gameBoard');
 
-for (i = 1; i <= 32; i++) {
-    tiles.push({
-        tileNum: i,
-        src: 'img/tile' + i + '.jpg',
-        flipped: false,
-        matched: false
-    });
-} // for each tile
+function makeTiles() {
+
+    for (i = 1; i <= 32; i++) {
+        tiles.push({
+            tileNum: i,
+            src: 'img/tile' + i + '.jpg',
+            flipped: false,
+            matched: false
+        });
+        } // for each tile
+    }
 
 // when document is ready
 $(document).ready(function() {
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    })
+    
     $('#startGame').click(function() {
         gameReset();
+        status();
+        makeTiles();
         gameSet();
+        console.log("matches: " + matches + " misses: " + misses + " remaining: " + remaining);
 
         // Starts timer
         var seconds = Date.now();
@@ -42,18 +54,21 @@ $(document).ready(function() {
 });
 
 function gameSet() {
+    //gets all tiles, chooses 8, clones the 8 tiles into pairs
+    // shuffles the 16 tiles onto the board
     tiles = _.shuffle(tiles);
     var toUse = tiles.slice(0, 8);
     var pairs = [];
-    _.forEach(toUse, function(tile) {
+    _.forEach(toUse, function (tile) {
         pairs.push(tile);
         pairs.push(_.clone(tile));
-    });
+    })
     pairs = _.shuffle(pairs);
+    console.log(pairs);
 
     var img;
     var row = $(document.createElement('div'));
-    _.forEach(pairs, function(tile, elemIndex) {
+    _.forEach(pairs, function (tile, elemIndex) {
         if (elemIndex > 0 && elemIndex % 4 == 0) {
             gameBoard.append(row);
             row = $(document.createElement('div'));
@@ -63,6 +78,7 @@ function gameSet() {
             src: 'img/tile-back.png',
             alt: 'tile ' + tile.tileNum
         });
+        //
         img.data('tile', tile);
         row.append(img);
     });
@@ -70,26 +86,71 @@ function gameSet() {
 }
 
 function gameReset() {
+    twoFlipped = false;
     gameBoard.empty();
-    click = true;
+    tiles = [];
+    pairs = [];
+    tempTile = [];
+    tempImage = [];
+    matches = 0;
+    misses = 0;
+    remaining = 8;
 }
 
 function gameRun() {
-   $('#gameBoard img').click(function() {
-        if(click) {
-            var clickedImg = $(this);
-            var tile = clickedImg.data('tile');
-            if (!tile.flipped) {
-                flipTile(tile, clickedImg);
-                console.log("Selected tile: " + tile.tileNum); 
-            }
-        }
-    });
+ $('#gameBoard img').click(function() {
+    var clickedImg = $(this);
+    var tile = clickedImg.data('tile');
+    if (twoFlipped == false && !tile.flipped) {
+        flipTile(tile, clickedImg);
+        console.log("Selected tile: " + tile.tileNum); 
+        compareTiles(tile, clickedImg);
+    }
+
+    status();
+
+    if(matches == 8){
+        setTimeout(function() {
+            gameBoard.empty();
+            clearInterval(timer);
+        }, 3000)
+    };
+});
 }
 
-// function to handle game status
+function status() {
+    $('#matches').text(matches);
+    $('#misses').text(misses);
+    $('#remaining').text(remaining);
+}
 
-// function to handle tile comparison
+function compareTiles(tile, img) {
+    tempTile.push(tile);
+    tempImage.push(img);
+
+    if (tempTile.length == 2) {
+        // Match
+        if (tempTile[0].src == tempTile[1].src) {
+            tempTile[0].matched = true;
+            tempTile[1].matched = true;
+            remaining--;
+            matches++;
+            tempTile = [];
+            tempImage = [];
+        // No match
+    } else {
+        twoFlipped = true;
+        misses++;
+        window.setTimeout(function() {
+            flipTile(tempTile[0], tempImage[0]);
+            flipTile(tempTile[1], tempImage[1]);
+            twoFlipped = false;
+            tempTile = [];
+            tempImage = [];
+        }, 1000);
+    }
+}
+}
 
 function flipTile(tile, img) {
     img.fadeOut(100, function() {
@@ -103,6 +164,3 @@ function flipTile(tile, img) {
     });
 }
 
-function flipBack(tile, img) {
-    tile.flipped = !tile.flipped;
-}
